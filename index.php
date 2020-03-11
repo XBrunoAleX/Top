c1ab0b95c52d3413476b6038d60725bfe5b5e0a8
Bruno Alex Insaurrauld Barros <insaurrauld.bruno@gmail.com>
2020-03-11 02:42:25 +0000

    bruno

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Brunihno</title>
+    <title>Bruno</title>
 </head>
 <body>
-    <h1>topss</h1>
-    bruno
+    <?php
+        $bruno =10;
+        echo $bruno;
+    ?>
+    
 </body>
 </html>
